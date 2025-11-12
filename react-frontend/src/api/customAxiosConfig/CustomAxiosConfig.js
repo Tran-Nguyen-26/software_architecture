0e8d9be44d05ac3d56b3904a2567ad7f49467588
Tran-Nguyen-26 <trannguyeen2005@gmail.com>
2025-11-13 00:26:23 +0700

    fix fe

--- react-frontend/src/api/customAxiosConfig/CustomAxiosConfig.js
+++ react-frontend/src/api/customAxiosConfig/CustomAxiosConfig.js
@@ -10,6 +10,12 @@ axiosInstance.interceptors.request.use((config) => {
   // add token to request headers
   config.headers["Authorization"] = localStorage.getItem("token");
   return config;
+
+  // const token = localStorage.getItem("token")
+  // if (token) {
+  //   config.headers["Authorization"] = `Bearer ${token}`; // thêm prefix Bearer
+  // }
+  // return config;
 });
 
 export default axiosInstance;

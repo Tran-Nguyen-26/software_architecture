1221c2288986ef55457405fe5656f79db4f086ce
Tran-Nguyen-26 <trannguyeen2005@gmail.com>
2025-12-09 18:03:34 +0700

    rate limit && circuit breaker

--- spring-backend/src/main/java/backend/hobbiebackend/service/impl/NotificationServiceImpl.java
+++ spring-backend/src/main/java/backend/hobbiebackend/service/impl/NotificationServiceImpl.java
@@ -2,24 +2,22 @@ package backend.hobbiebackend.service.impl;
 
 import backend.hobbiebackend.model.entities.UserEntity;
 import backend.hobbiebackend.service.NotificationService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
     private final JavaMailSender javaMailSender;
 
-    @Autowired
     public NotificationServiceImpl(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
     @Override
+    @CircuitBreaker(name = "emailService", fallbackMethod = "emailFallback")
     public void sendNotification(UserEntity userEntity) {
         SimpleMailMessage mail = new SimpleMailMessage();
         String mailBody = "http://localhost:4200/password/" + userEntity.getId();
@@ -29,5 +27,10 @@ public class NotificationServiceImpl implements NotificationService {
         mail.setText("Click the link to reset your password: " + mailBody);
 
         javaMailSender.send(mail);
+    }
+
+    public void emailFallback(UserEntity userEntity, Throwable t) {
+        System.out.println("Fallback triggered for user: " + userEntity.getEmail());
+        System.out.println("Reason: " + t.getMessage());
     }
 }
